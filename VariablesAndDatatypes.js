a9bc818bc17d0f48ce0eb769c41100ce49a442d0
Sohith Bandreddi <sohith73@gmail.com>
2023-10-15 15:48:53 +0530

    know about variables and data types

---
+++ VariablesAndDatatypes.js
@@ -0,0 +1,14 @@
+const a = 29 // declaring a variable from const 
+console.log(a);
+var b = 90;// never declare a variable as var in js it is a bad practice 
+console.log(b);
+c  = 42; // declare a variable without anything
+console.table([a,b,c]);
+
+
+//number is a data type we know it as integer
+//string is a data type we know it as string
+//boolean is a data type we know it as boolean
+//null is a data type we know it as empty 
+//undefined is a data type we just declare variable name with no value 
+//symbol => unique one 
