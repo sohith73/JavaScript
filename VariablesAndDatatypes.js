045a607548c17f5c07193385a933cb695b0f7e3f
Sohith Bandreddi <sohith73@gmail.com>
2023-10-16 08:37:45 +0530

    Operations and Datatype

--- VariablesAndDatatypes.js
+++ VariablesAndDatatypes.js
@@ -12,9 +12,34 @@ c  = 42; // declare a variable without anything
 //null is a data type we know it as empty 
 //undefined is a data type we just declare variable name with no value 
 //symbol => unique one 
-let score = 90;
-console.log(typeof(score));  // lets see type conversions in js 
-score = "90"
-console.log(typeof score);
-let value = Number(score);
-console.log(typeof value);  // js converts anything '99' -> 99 '99a'-> nan(Not a number) 
+// let score = 90;
+// console.log(typeof(score));  // lets see type conversions in js 
+// score = "90"
+// console.log(typeof score);
+// let value = Number(score);
+// console.log(typeof value);  // js converts anything '99' -> 99 '99a'-> nan(Not a number) 
+
+
+// --------------- operations ---------------
+
+// let score = 90
+// console.log(-score); // console output will be negative of score
+// let s1 = "Sohith"
+// let s2 = " Bandreddi"
+// console.log(s1+s2);
+
+// --------------- Data types ---------------
+//js have 7 primitive types they just pass a copy that can change 
+//Integer(Number),String,Boolean,null,undefined,symbol,Bigint
+//reference(Non primitive types): they have one memory address
+// array, object, function
+
+// let data = [1,2,3,4,5,6,7,8,9,10];
+// let onj1  = {
+//      name : "Sohith",
+//      age : 20,
+// };
+// let myFUnction = function(){
+//      console.log("Hello world");
+// }
+
