0c8ae8b2170fdeaa1a181a145e79a49d9abf354e
Sohith Bandreddi <sohith73@gmail.com>
2023-10-16 21:06:37 +0530

    Started arrays

---
+++ array.js
@@ -0,0 +1,6 @@
+const op = [1,2,3,4,5,6,7,8,9] // normal way of using array
+// console.log(op[5]);
+op.push(1);//add element at the end
+console.log(op);
+op.pop();//remove element at the end
+console.log(op);
