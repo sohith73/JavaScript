ee86493f19dbf20e529e8fa96de5c0dfd5bb3de8
Sohith Bandreddi <sohith73@gmail.com>
2023-10-16 10:50:12 +0530

    added string concept

---
+++ String.js
@@ -0,0 +1,7 @@
+console.log("Hello world!");
+//String interpolation
+console.log(`Hello world`);// which acts as f string in python
+let a = 10
+let score =100
+let userName = "Sohith Bandreddi"
+console.log(`HI ${userName} your current score is ${score} and added score ${a} so now total score is ${score+a}`);
