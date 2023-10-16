ada1f27264a24bae4216365e5086b39d8b4a894b
Sohith Bandreddi <sohith73@gmail.com>
2023-10-16 08:05:43 +0530

    type conversions in js and learned new things in

--- VariablesAndDatatypes.js
+++ VariablesAndDatatypes.js
@@ -1,9 +1,9 @@
 const a = 29 // declaring a variable from const 
-console.log(a);
+// console.log(a);
 var b = 90;// never declare a variable as var in js it is a bad practice 
-console.log(b);
+// console.log(b);
 c  = 42; // declare a variable without anything
-console.table([a,b,c]);
+// console.table([a,b,c]);
 
 
 //number is a data type we know it as integer
@@ -12,3 +12,9 @@ console.table([a,b,c]);
 //null is a data type we know it as empty 
 //undefined is a data type we just declare variable name with no value 
 //symbol => unique one 
+let score = 90;
+console.log(typeof(score));  // lets see type conversions in js 
+score = "90"
+console.log(typeof score);
+let value = Number(score);
+console.log(typeof value);  // js converts anything '99' -> 99 '99a'-> nan(Not a number) 
