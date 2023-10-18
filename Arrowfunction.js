e1c899f73537f2f3490d597db712902fe78cd444
Sohith Bandreddi <sohith73@gmail.com>
2023-10-18 11:52:28 +0530

    arrow function

--- Arrowfunction.js
+++ Arrowfunction.js
@@ -19,5 +19,20 @@ const bsc = () => {  // intro to arrow function remove that function and keep =>
      let userName = "Sohith 96";
      console.log(userName);
 }
-bsc();
-BSC();
+// bsc();
+// BSC();
+
+() => {} // arrow function  
+// const add = (num,num1) => {  basic arrow function 
+//      return num+num1;
+// }
+// this is implicit return  no need of {} and return statement
+// const add = (num,num1) => num+num1;
+const add = (num,num1) => (num+num1); // no need of return when we use () this we need return for this{}
+console.log(add(19,1));
+
+// for returning a class we can rap it in ({class})
+
+const clas = (num,num1) => ({username : "Sohith"});
+console.log(clas(2,3));
+
